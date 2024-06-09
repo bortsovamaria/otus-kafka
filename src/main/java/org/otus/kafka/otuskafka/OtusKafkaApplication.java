bd005e9fc247bb7fc1d8280bae60659588f60f95
Maria Bortsova <bortsovamariia@gmail.com>
2024-06-09 19:51:50 +0300

    init

---
+++ src/main/java/org/otus/kafka/otuskafka/OtusKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.otus.kafka.otuskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtusKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/otus/kafka/otuskafka/OtusKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.otus.kafka.otuskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
